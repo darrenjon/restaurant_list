de3c55679715106486756ac493f51450eaf4901d
darrenjon <darrenlin23@gmail.com>
2022-08-02 23:31:10 +0800

    feat: add update restaurant list function

--- app.js
+++ app.js
@@ -45,13 +45,43 @@ app.post('/restaurants', (req, res) => {
 })
 
 // add read more details of restaurant
-app.get('/restaurants/:restaurantId', (req, res) => {
-  const id = req.params.restaurantId
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
   return Restaurant.findById(id)
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(err => console.log(err))
 })
+
+// add edit function and page
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(err => console.log(err))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = body.name
+      restaurant.name_en = body.name_en
+      restaurant.category = body.category
+      restaurant.image = body.image
+      restaurant.location = body.location
+      restaurant.phone = body.phone
+      restaurant.google_map = body.google_map
+      restaurant.rating = body.rating
+      restaurant.description = body.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect('/restaurants/${id}'))
+    .catch(err => console.log(err))
+})
+
 
 app.get('/search', (req, res) => {
   const input = req.query.keyword
