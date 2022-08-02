aaf451b04218e71697886bad5669c04ae5c006a1
darrenjon <darrenlin23@gmail.com>
2022-08-02 20:17:12 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../../restaurant.json').results
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log('mongodb connected!')
+    })
+    .catch(error => console.log(error))
+})
