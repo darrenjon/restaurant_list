981315ef24cf62ddd77533acf8aed9cde6182076
darrenjon <darrenlin23@gmail.com>
2022-08-25 23:20:35 +0800

    refactor: remove name validation

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,7 +23,7 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
+  if (!email || !password || !confirmPassword) {
     errors.push({ message: 'Registration failed! Please complete all the required fields to proceed.' })
   }
   if (password !== confirmPassword) {
